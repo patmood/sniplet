aca68987a40273f1928dc62794649d04e2dd73fe
patmood <patrick.n.moody@gmail.com>
2015-04-29 15:01:04 +0100

    push new post onto list

--- public/javascripts/controllers/MainController.js
+++ public/javascripts/controllers/MainController.js
@@ -5,4 +5,13 @@ angular.module('MainController', ['post']).controller('MainController', function
   })
   $scope.tagline = 'To the moon and back!';
 
+  $scope.createPost = function() {
+    var post = {
+      title: $scope.post.title,
+      body: $scope.post.body,
+      author: $scope.post.author
+    }
+    $scope.posts.push(post)
+  }
+
 });
