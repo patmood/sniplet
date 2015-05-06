cd0cc0217e5a3c862792b3a781913cf575cbae5b
patmood <patrick.n.moody@gmail.com>
2015-05-06 11:56:51 +0100

    gulp livereload with nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp')
   , sass = require('gulp-sass')
+  , nodemon = require('gulp-nodemon')
+
+// Plugins
+var livereload = require('gulp-livereload')
 
 gulp.task('watch', function() {
+  livereload.listen();
   gulp.watch('./src/**/*.scss', ['sass'])
 })
 
@@ -9,6 +14,9 @@ gulp.task('sass', function() {
   gulp.src('./src/stylesheets/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('./public/stylesheets'))
+    .pipe(livereload())
 })
 
-gulp.task('default', ['watch', 'sass'])
+gulp.task('default', ['watch', 'sass'], function() {
+  nodemon()
+})
